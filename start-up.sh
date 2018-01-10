55fbeacfdc3fabfb2a78bb0d05ac117b4ec47f65
U-NAEAST\D569906 <D569906@v10012000.NAEAST.AD.JPMORGANCHASE.com>
2018-01-10 12:09:57 -0300

    initial commit

---
+++ start-up.sh
@@ -0,0 +1,11 @@
+
+#
+# git clone my repo
+# ./start-up.sh
+#
+# kubelet --network-plugin=cni --network-plugin-dir=/etc/cni/net.d
+mkdir /tmp/data
+kubectl create -f pv-volume.yaml
+kubectl create -f pv-claim.yaml
+kubectl create secret generic mysql-pass --from-literal=password=123456qwe
+kubectl create -f mysql-deployment.yaml
