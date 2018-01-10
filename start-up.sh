f7c48c26c0b0a01b66bfa96fb532e51f7f7a8f56
U-NAEAST\D569906 <D569906@v10012000.NAEAST.AD.JPMORGANCHASE.com>
2018-01-10 12:56:17 -0300

    changed

--- start-up.sh
+++ start-up.sh
@@ -1,14 +1,12 @@
 
 #
-# git clone my repo
+# git clone 
 # ./start-up.sh
 #
 # kubelet --network-plugin=cni --network-plugin-dir=/etc/cni/net.d
-# mkdir /home/test
-# cd /home/test
 
 mkdir /tmp/data
 kubectl create -f pv-volume.yaml
-kubectl create -f pv-claim.yaml
+# kubectl create -f pv-claim.yaml
 kubectl create secret generic mysql-pass --from-literal=password=123456qwe
 kubectl create -f mysql-deployment.yaml
