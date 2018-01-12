fabd9cb743c2745fc849ca04471ac5de78cadca8
D569906 <wuelfhis.asuaje@jpmorgan.com>
2018-01-12 15:27:10 -0300

    new change

--- start-up.sh
+++ start-up.sh
@@ -8,7 +8,7 @@
 # docker run wasuaje/app-flask-mysql 
 # git clone https://github.com/wasuaje/kube-test.git
 mkdir /tmp/data
-kubectl create -f pv-volume.yaml
+#kubectl create -f pv-volume.yaml
 # kubectl create -f pv-claim.yaml
 kubectl create secret generic mysql-pass --from-literal=password=123456qwe
 kubectl create -f mysql-deployment.yaml
