a8f20075b7f26d38a759e5fb4cc953dd250b82d3
D569906 <wuelfhis.asuaje@jpmorgan.com>
2018-01-12 10:46:21 -0300

    new change

--- app-flask/app.py
+++ app-flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json, request
-from flask.ext.mysql import MySQL
+from flask_mysql import MySQL
 
 
 app = Flask(__name__)
--- start-up.sh
+++ start-up.sh
@@ -4,6 +4,9 @@
 # ./start-up.sh
 #
 # kubelet --network-plugin=cni --network-plugin-dir=/etc/cni/net.d
+# docker build -t wasuaje/app-flask-mysql app-flask/
+# docker push wasuaje/app-flask-mysql 
+git clone https://github.com/wasuaje/kube-test.git
 
 mkdir /tmp/data
 kubectl create -f pv-volume.yaml
@@ -16,5 +19,4 @@ kubectl create configmap nginxconfigmap --from-file=http-nginx/default.conf
 kubectl expose service nginxsvc --type=LoadBalancer --name=service-test-app
 #kubectl expose pod nginx-pv-pod --type=LoadBalancer --name=lb-nginx-test-app
 
-docker build -t tiao/gb-frontend-flask-redis guestbook-flask/flask-redis
-docker push tiao/gb-frontend-flask-redis
+
