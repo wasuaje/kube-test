62b6d7ee24e14c162fedf7d15f9cb3396296afe9
D569906 <wuelfhis.asuaje@jpmorgan.com>
2018-01-15 13:05:44 -0300

    new change

--- setup.sh
+++
@@ -1,6 +0,0 @@
-kubeadm init --apiserver-advertise-address $(hostname -i)
-kubectl apply -n kube-system -f     "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-cd $HOME
