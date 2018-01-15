b8101d1d6642811cdcfe9496ab0faf8345e4c368
D569906 <wuelfhis.asuaje@jpmorgan.com>
2018-01-15 10:07:52 -0300

    new change

--- start-up.sh
+++ start-up.sh
@@ -2,11 +2,11 @@
 #
 # start-up.sh
 #
-# kubelet --network-plugin=cni --network-plugin-dir=/etc/cni/net.d
+# Commands for image managing
 # docker build -t wasuaje/app-flask-mysql app-flask/
 # docker push wasuaje/app-flask-mysql 
 # docker run wasuaje/app-flask-mysql 
-# yum install -y git vim wget dos2unix
+# yum install -y git vim dos2unix
 # git clone https://github.com/wasuaje/kube-test.git
 
 mkdir /tmp/data
@@ -19,7 +19,10 @@ kubectl create configmap nginxconfigmap --from-file=http-nginx/default.conf
 # kubectl expose service nginxsvc --type=LoadBalancer --name=service-test-app
 #kubectl expose pod nginx-pv-pod --type=LoadBalancer --name=lb-nginx-test-app
 kubectl create -f flask-deployment.yaml
+kubectl create -f nginx-rc.yaml
+
 # worked exposing backend
 kubectl expose service flask-app --type=LoadBalancer --port=8080 --target-port=8080 --name=flask-exposed
+kubectl expose service fnginxsvc --type=LoadBalancer --port=80 --target-port=80 --name=nginx-exposed
 
 
