a9d67d490784a2c2cedc64dcbe4b8ff2b2bff35c
D569906 <wuelfhis.asuaje@jpmorgan.com>
2018-01-17 17:21:36 -0300

    small change

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,11 @@
-kubectl delete deployment -l app=test-app
-kubectl delete service -l app=test-app
-kubectl delete pvc -l app=test-app
+#!/usr/bin/bash
+
+kubectl delete secret mysql-pass
+kubectl delete -f mysql-deployment.yaml
+kubectl delete configmap nginxconfigmap 
+kubectl delete -f flask-deployment.yaml
+kubectl delete -f nginx-rc.yaml
+cd ..
+cd heapster
+kubectl delete -f deploy/kube-config/influxdb/
+kubectl delete -f deploy/kube-config/rbac/heapster-rbac.yaml
